dbfc26fca1dcea06505188f7eb0348c11c5ea2f9
Gus Cavanaugh <Gus Cavanaugh>
2016-08-05 22:16:31 -0400

    deleted chart route

--- app.py
+++ app.py
@@ -57,15 +57,6 @@ def signup():
             return render_template(index, chartID='chart_ID', chart=chart, series=series, title=title, xAxis=xAxis, yAxis=yAxis)
 
     return render_template(index)
-	
-@app.route('/chart', methods = ['GET', 'POST'])
-def chart(chartID = 'chart_ID', chart_type = 'bar', chart_height = 350):
-    chart = {"renderTo": chartID, "type": chart_type, "height": chart_height,}
-    series = [{"name": 'Label1', "data": [1,2,3]}, {"name": 'Label2', "data": [4, 5, 6]}]
-    title = {"text": 'My Title'}
-    xAxis = {"categories": ['xAxis Data1', 'xAxis Data2', 'xAxis Data3']}
-    yAxis = {"title": {"text": 'yAxis Label'}}
-    return render_template('chart.html', chartID=chartID, chart=chart, series=series, title=title, xAxis=xAxis, yAxis=yAxis)
 
 	
 if __name__ == "__main__":
